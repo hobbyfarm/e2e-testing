f5f962ce368d75e3b1b81035394a9ba2383c23eb
Bertrand Thomas <bertrand@devpro.fr>
2023-03-20 17:44:50 +0100

    Add test run in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,54 +29,59 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [ ['html', { open: 'never' }] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-
+    /* Ignore invalid certificates */
+    ignoreHTTPSErrors: true,
+    /* Take screenshots */
+    screenshot: 'off',
+    /* Take videos */
+    video: 'off',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'] }
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { ...devices['Desktop Safari'] }
     },
 
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
+    //   use: { ...devices['Pixel 5'] }
     // },
     // {
     //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
+    //   use: { ...devices['iPhone 12'] }
     // },
 
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
-    //   use: { channel: 'msedge' },
+    //   use: { channel: 'msedge' }
     // },
     // {
     //   name: 'Google Chrome',
-    //   use: { channel: 'chrome' },
-    // },
+    //   use: { channel: 'chrome' }
+    // }
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
@@ -85,6 +90,6 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  //   port: 3000
+  // }
 });
