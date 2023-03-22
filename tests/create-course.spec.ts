0ab740266b3fc88bf9c751d80743945175da4246
Michael <michael.hesemann@sva.de>
2023-03-22 09:37:43 +0100

    linted code

--- tests/create-course.spec.ts
+++ tests/create-course.spec.ts
@@ -5,7 +5,7 @@ test('create new course', async ({ page }) => {
   let loginPage = new LoginPage(page);
   await loginPage.goto(process.env.HOBBYFARM_ADMIN_UI_URL as string);
   const homePage = await loginPage.fillCredentialsAndSubmit(process.env.HOBBYFARM_ADMIN_UI_USR as string, process.env.HOBBYFARM_ADMIN_UI_PWD as string);
-  let contentManagementPage = await homePage.openContentManagementPage();
+  const contentManagementPage = await homePage.openContentManagementPage();
   await contentManagementPage.openNewCourseModalAndSaveAndDelete('testname' as string, 'testdescription' as string, '15' as string, '2' as string);
   loginPage = await homePage.logout();
 });
--- tests/create-scenario.spec.ts
+++ tests/create-scenario.spec.ts
@@ -2,10 +2,10 @@ import { test } from '@playwright/test';
 import { LoginPage } from '../src/pages/admin-ui';
 
 test('create new scenario', async ({ page }) => {
-    let loginPage = new LoginPage(page);
-    await loginPage.goto(process.env.HOBBYFARM_ADMIN_UI_URL as string);
-    let homePage = await loginPage.fillCredentialsAndSubmit(process.env.HOBBYFARM_ADMIN_UI_USR as string, process.env.HOBBYFARM_ADMIN_UI_PWD as string);
-    let contentManagementPage = await homePage.openContentManagementPage();
-    await contentManagementPage.openNewScenarioModalAndSave('testname' as string, 'testdescription' as string, '15' as string, '2' as string);
-    loginPage = await homePage.logout();
+  let loginPage = new LoginPage(page);
+  await loginPage.goto(process.env.HOBBYFARM_ADMIN_UI_URL as string);
+  const homePage = await loginPage.fillCredentialsAndSubmit(process.env.HOBBYFARM_ADMIN_UI_USR as string, process.env.HOBBYFARM_ADMIN_UI_PWD as string);
+  const contentManagementPage = await homePage.openContentManagementPage();
+  await contentManagementPage.openNewScenarioModalAndSave('testname' as string, 'testdescription' as string, '15' as string, '2' as string);
+  loginPage = await homePage.logout();
 });
