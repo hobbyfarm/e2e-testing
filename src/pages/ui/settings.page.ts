fc65a06cb3d18ece743b0c8039d0d61ae10f6ca3
Arne Schreiber <127104721+Arne232@users.noreply.github.com>
2023-04-05 13:50:12 +0200

    added change terminal theme test

--- src/pages/ui/settings.page.ts
+++ src/pages/ui/settings.page.ts
@@ -3,140 +3,158 @@ import { Locator, Page, expect } from '@playwright/test';
 export class SettingsPage {
   private accessCode: string = 'e2e-testing' + Math.random();
   readonly page: Page;
-  readonly profilMAC: Locator;
-  readonly profilACField: Locator;
+  readonly profileMAC: Locator;
+  readonly profileACField: Locator;
   readonly manageAccessCodesInput: Locator;
-  readonly profilAAC: Locator;
-  readonly profilSAAC: Locator;
-  readonly profilErrorAC: Locator;
-  readonly profilDelete: Locator;
-  readonly profilClose: Locator;
-  readonly profilSave: Locator;
-  readonly profilSettings: Locator;
-  readonly profilFontSizeField: Locator;
-  readonly profilFontSize: Locator;
-  readonly profilChangePassword: Locator;
-  readonly profilOldPassword: Locator;
-  readonly profilNewPassword: Locator;
-  readonly profilNewPasswordAgain: Locator;
-  readonly profilChangePasswordB: Locator;
+  readonly profileAAC: Locator;
+  readonly profileSAAC: Locator;
+  readonly profileErrorAC: Locator;
+  readonly profileDelete: Locator;
+  readonly profileClose: Locator;
+  readonly profileSave: Locator;
+  readonly profileSettings: Locator;
+  readonly profileFontSizeField: Locator;
+  readonly profileFontSize: Locator;
+  readonly profileChangePassword: Locator;
+  readonly profileOldPassword: Locator;
+  readonly profileNewPassword: Locator;
+  readonly profileNewPasswordAgain: Locator;
+  readonly profileChangePasswordB: Locator;
+  readonly profileTerminalTheme: Locator;
+  readonly profileTerminalThemeNew: Locator;
+  readonly profileTerminalThemeOld: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.profilMAC = page.getByRole('menuitem', { name: 'Manage Access Codes' });
-    this.profilAAC = page.getByRole('button', { name: 'Add Access Code' });
-    this.profilACField = page.getByLabel('Access Code', { exact: true });
+    this.profileMAC = page.getByRole('menuitem', { name: 'Manage Access Codes' });
+    this.profileAAC = page.getByRole('button', { name: 'Add Access Code' });
+    this.profileACField = page.getByLabel('Access Code', { exact: true });
     this.manageAccessCodesInput = page.getByLabel('Access Code', { exact: true });
-    this.profilSAAC = page.getByRole('button', { name: 'Save & Activate' });
-    this.profilErrorAC = page.getByText('error adding access code');
-    this.profilDelete = page.getByRole('button', { name: 'Delete' });
-    this.profilClose = page.getByText('Close');
-    this.profilSave = page.getByRole('button', { name: 'Save' });
-    this.profilSettings = page.getByRole('menuitem', { name: 'Settings' });
-    this.profilFontSizeField = page.getByLabel('Font-Size');
-    this.profilFontSize = page.getByLabel('Font-Size');
-    this.profilChangePassword = page.getByRole('menuitem', { name: 'Change Password' });
-    this.profilOldPassword = page.getByLabel('Old Password');
-    this.profilNewPassword = page.getByLabel('New Password').nth(0);
-    this.profilNewPasswordAgain = page.getByLabel('New Password Again');
-    this.profilChangePasswordB = page.getByRole('button', { name: 'Change Password' });
+    this.profileSAAC = page.getByRole('button', { name: 'Save & Activate' });
+    this.profileErrorAC = page.getByText('error adding access code');
+    this.profileDelete = page.getByRole('button', { name: 'Delete' });
+    this.profileClose = page.getByText('Close');
+    this.profileSave = page.getByRole('button', { name: 'Save' });
+    this.profileSettings = page.getByRole('menuitem', { name: 'Settings' });
+    this.profileFontSizeField = page.getByLabel('Font-Size');
+    this.profileFontSize = page.getByLabel('Font-Size');
+    this.profileChangePassword = page.getByRole('menuitem', { name: 'Change Password' });
+    this.profileOldPassword = page.getByLabel('Old Password');
+    this.profileNewPassword = page.getByLabel('New Password').nth(0);
+    this.profileNewPasswordAgain = page.getByLabel('New Password Again');
+    this.profileChangePasswordB = page.getByRole('button', { name: 'Change Password' });
+    this.profileTerminalTheme = page.getByRole('tab', { name: 'Terminal Theme' });
+    this.profileTerminalThemeNew = page.locator('div:nth-child(2) > .theme-preview-radio-box > .theme-preview-container > .theme-preview-terminal');
+    this.profileTerminalThemeOld = page.locator('.theme-preview-terminal').first();
   }
 
-  async openProfil(user: string) {
+  async openProfile(user: string) {
     await this.page.getByRole('button', { name: user as string }).click();
   }
 
-  async openProfilMAC() { //Profil ManageAccessCodes
-    await this.profilMAC.click();
+  async openProfileMAC() { //profile ManageAccessCodes
+    await this.profileMAC.click();
   }
 
-  async openProfilAAC() { // Profil AddAccessCode
-    await this.profilAAC.click();
+  async openProfileAAC() { // profile AddAccessCode
+    await this.profileAAC.click();
   }
 
-  async openProfilACField() { // Profil AccessCodeFeld
-    await this.profilACField.click();
+  async openProfileACField() { // profile AccessCodeFeld
+    await this.profileACField.click();
   }
 
   async accessCodeInput() {
     await this.manageAccessCodesInput.fill(this.accessCode);
   }
 
-  async openProfilSAAC() { // Profil Save&ActivateAccessCode
-    await this.profilSAAC.click();
+  async openProfileSAAC() { // profile Save&ActivateAccessCode
+    await this.profileSAAC.click();
   }
 
   async openErrorAC() {
-    await this.profilErrorAC.click();
+    await this.profileErrorAC.click();
   }
 
   async openDelete() {
-    await this.profilDelete.click();
+    await this.profileDelete.click();
   }
 
   async openClose() {
-    await this.profilClose.click();
+    await this.profileClose.click();
   }
 
-  async openProfilSettings() {
-    await this.profilSettings.click();
+  async openProfileSettings() {
+    await this.profileSettings.click();
   }
 
   async openFontSizeField() {
-    await this.profilFontSizeField.click();
+    await this.profileFontSizeField.click();
   }
 
-  async openProfilFontSizeUp() {
-    await this.profilFontSize.press('ArrowUp');
+  async openProfileFontSizeUp() {
+    await this.profileFontSize.press('ArrowUp');
   }
 
-  async openProfilFontSizeDown() {
-    await this.profilFontSize.press('ArrowDown');
+  async openProfileFontSizeDown() {
+    await this.profileFontSize.press('ArrowDown');
   }
 
-  async openProfilSave() {
-    await this.profilSave.click();
+  async openProfileSave() {
+    await this.profileSave.click();
   }
 
-  async openProfilChangePassword() {
-    await this.profilChangePassword.click();
+  async openProfileChangePassword() {
+    await this.profileChangePassword.click();
   }
 
-  async openProfilOldPasswordField() {
-    await this.profilOldPassword.click();
+  async openProfileOldPasswordField() {
+    await this.profileOldPassword.click();
   }
 
-  async openProfilOldPasswordInput(oldPassword: string) {
-    await this.profilOldPassword.fill(oldPassword);
+  async openProfileOldPasswordInput(oldPassword: string) {
+    await this.profileOldPassword.fill(oldPassword);
   }
 
-  async openProfilNewPasswordField() {
-    await this.profilNewPassword.click();
+  async openProfileNewPasswordField() {
+    await this.profileNewPassword.click();
   }
 
-  async openProfilNewPasswordInput(newPassword: string) {
-    await this.profilNewPassword.fill(newPassword);
+  async openProfileNewPasswordInput(newPassword: string) {
+    await this.profileNewPassword.fill(newPassword);
   }
 
-  async openProfilNewPasswordAgainField() {
-    await this.profilNewPasswordAgain.click();
+  async openProfileNewPasswordAgainField() {
+    await this.profileNewPasswordAgain.click();
   }
 
-  async openProfilNewPasswordAgainInput(newPasswordAgain: string) {
-    await this.profilNewPasswordAgain.fill(newPasswordAgain);
+  async openProfileNewPasswordAgainInput(newPasswordAgain: string) {
+    await this.profileNewPasswordAgain.fill(newPasswordAgain);
   }
 
-  async openProfilChangePasswordB() {
-    await this.profilChangePasswordB.click();
+  async openProfileChangePasswordB() {
+    await this.profileChangePasswordB.click();
+  }
+
+  async openProfileTerminalTheme() {
+    await this.profileTerminalTheme.click();
+  }
+
+  async openProfileTerminalThemeNew() {
+    await this.profileTerminalThemeNew.click();
+  }
+
+  async openProfileTerminalThemeOld() {
+    await this.profileTerminalThemeOld.click();
   }
 
   async openAccessCodeAddSaveDelete(user: string): Promise<SettingsPage> {
-    await this.openProfil(user as string);
-    await this.openProfilMAC();
-    await this.openProfilAAC();
-    await this.openProfilACField();
+    await this.openProfile(user as string);
+    await this.openProfileMAC();
+    await this.openProfileAAC();
+    await this.openProfileACField();
     await this.accessCodeInput();
-    await this.openProfilSAAC();
+    await this.openProfileSAAC();
     await expect(this.page.getByText(this.accessCode + ' added.')).toHaveText(this.accessCode + ' added.' as string);
     await expect(this.page.getByRole('gridcell', { name: this.accessCode })).toHaveText(this.accessCode as string);
     await this.page.getByRole('row', { name: 'Available actions ' + this.accessCode + ' No associated event' }).getByRole('button', { name: 'Available actions' }).locator('svg').click();
@@ -146,18 +164,19 @@ export class SettingsPage {
   }
 
   async openAccessCodeUnique(user: string): Promise<SettingsPage> {
-    await this.openProfil(user as string);
-    await this.openProfilMAC();
-    await this.openProfilAAC();
-    await this.openProfilACField();
+    await this.openProfile(user as string);
+    await this.openProfileMAC();
+    await this.openProfileAAC();
+    await this.openProfileACField();
     await this.accessCodeInput();
-    await this.openProfilSAAC();
+    await this.openProfileSAAC();
     await expect(this.page.getByText('Add Access Code')).toHaveText('Add Access Code' as string);
-    await this.openProfilAAC();
-    await this.openProfilAAC();
-    await this.openProfilACField();
+    for (let i = 0; i < 10; i++) {
+      await this.openProfileAAC();
+    }
+    await this.openProfileACField();
     await this.accessCodeInput();
-    await this.openProfilSAAC();
+    await this.openProfileSAAC();
     await this.openErrorAC();
     await expect(this.page.getByRole('gridcell', { name: this.accessCode })).toHaveText(this.accessCode as string);
     await this.page.getByRole('row', { name: 'Available actions ' + this.accessCode + ' No associated event' }).getByRole('button', { name: 'Available actions' }).locator('svg').click();
@@ -167,42 +186,55 @@ export class SettingsPage {
   }
 
   async openSettingsChangeFontSize(user: string): Promise<SettingsPage> {
-    await this.openProfil(user as string);
-    await this.openProfilSettings();
+    await this.openProfile(user as string);
+    await this.openProfileSettings();
     await this.openFontSizeField();
-    await this.openProfilFontSizeUp();
-    await this.openProfilSave();
-    await this.openProfil(user as string);
-    await this.openProfilSettings();
+    await this.openProfileFontSizeUp();
+    await this.openProfileSave();
+    await this.openProfile(user as string);
+    await this.openProfileSettings();
     await this.openFontSizeField();
-    await this.openProfilFontSizeDown();
-    await this.openProfilSave();
+    await this.openProfileFontSizeDown();
+    await this.openProfileSave();
     return new SettingsPage(this.page);
   }
 
   async openPasswordChangeTest(user: string, oldPassword: string, newPassword: string): Promise<SettingsPage> {
-    await this.openProfil(user as string);
-    await this.openProfilChangePassword();
-    await this.openProfilOldPasswordField();
-    await this.openProfilOldPasswordInput(oldPassword as string);
-    await this.openProfilNewPasswordField();
-    await this.openProfilNewPasswordInput(newPassword as string);
-    await this.openProfilNewPasswordAgainField();
-    await this.openProfilNewPasswordAgainInput(newPassword as string);
-    await this.openProfilChangePasswordB();
+    await this.openProfile(user as string);
+    await this.openProfileChangePassword();
+    await this.openProfileOldPasswordField();
+    await this.openProfileOldPasswordInput(oldPassword as string);
+    await this.openProfileNewPasswordField();
+    await this.openProfileNewPasswordInput(newPassword as string);
+    await this.openProfileNewPasswordAgainField();
+    await this.openProfileNewPasswordAgainInput(newPassword as string);
+    await this.openProfileChangePasswordB();
     return new SettingsPage(this.page);
   }
 
   async openPasswordChangeBack(user: string, oldPassword: string, newPassword: string): Promise<SettingsPage> {
-    await this.openProfil(user as string);
-    await this.openProfilChangePassword();
-    await this.openProfilOldPasswordField();
-    await this.openProfilOldPasswordInput(newPassword as string);
-    await this.openProfilNewPasswordField();
-    await this.openProfilNewPasswordInput(oldPassword as string);
-    await this.openProfilNewPasswordAgainField();
-    await this.openProfilNewPasswordAgainInput(oldPassword as string);
-    await this.openProfilChangePasswordB();
+    await this.openProfile(user as string);
+    await this.openProfileChangePassword();
+    await this.openProfileOldPasswordField();
+    await this.openProfileOldPasswordInput(newPassword as string);
+    await this.openProfileNewPasswordField();
+    await this.openProfileNewPasswordInput(oldPassword as string);
+    await this.openProfileNewPasswordAgainField();
+    await this.openProfileNewPasswordAgainInput(oldPassword as string);
+    await this.openProfileChangePasswordB();
+    return new SettingsPage(this.page);
+  }
+
+  async openSettingsChangeTheme(user: string): Promise<SettingsPage> { //Change Terminal Theme
+    await this.openProfile(user as string);
+    await this.openProfileSettings();
+    await this.openProfileTerminalTheme();
+    await this.openProfileTerminalThemeNew();
+    await this.openProfileSave();
+    await this.openProfile(user as string);
+    await this.openProfileSettings();
+    await this.openProfileTerminalThemeOld();
+    await this.openProfileSave();
     return new SettingsPage(this.page);
   }
 }
