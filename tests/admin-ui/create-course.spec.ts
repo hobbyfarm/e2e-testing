db4c24e0fa7b63ab0c45f9642fdb021747e59f01
Bertrand Thomas <bertrand@devpro.fr>
2023-05-11 14:58:32 +0200

    Factorize login in ui tests

--- tests/admin-ui/create-course.spec.ts
+++ tests/admin-ui/create-course.spec.ts
@@ -1,10 +1,8 @@
 import { test } from '@playwright/test';
-import { LoginPage } from '../../src/pages/admin-ui';
+import { AdminUiFlow } from './admin-ui.flow';
 
 test('Create new course', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto(process.env.HOBBYFARM_ADMIN_UI_URL as string);
-  const homePage = await loginPage.fillCredentialsAndSubmit(process.env.HOBBYFARM_ADMIN_UI_USR as string, process.env.HOBBYFARM_ADMIN_UI_PWD as string);
+  const homePage = await AdminUiFlow.login(page);
   const contentManagementPage = await homePage.openContentManagementPage();
   await contentManagementPage.openNewCourseModalAndSave(`testname ${new Date().toLocaleString('en-GB', { timeZone: 'UTC' })}`, 'testdescription', '15', '2');
   await contentManagementPage.logout();
