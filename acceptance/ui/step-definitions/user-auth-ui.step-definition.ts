31ad94420ac7fd2f12dba5d3c731842517008d23
Abdellah Brahimi <abdellah.brahimi@sva.de>
2023-05-15 15:03:09 +0200

    Changing the name of variable for UI from HOBBYFARM_ADMIN_UI_URL to HOBBYFARM_UI_URL

--- acceptance/ui/step-definitions/user-auth-ui.step-definition.ts
+++ acceptance/ui/step-definitions/user-auth-ui.step-definition.ts
@@ -13,7 +13,7 @@ export class UserAuthStepDefinition extends BaseStepDefinition {
   @given(/I am on the UI login page/, 'UI', 100000)
   public async givenLoginPageIsOpened(): Promise<void> {
     const loginPage = new LoginPage(this.sessionContext.page ?? (() => { throw new Error('page is null'); })());
-    await loginPage.goto(process.env.HOBBYFARM_ADMIN_UI_URL as string);
+    await loginPage.goto(process.env.HOBBYFARM_UI_URL as string);
     this.sessionContext.current = loginPage;
   }
 
@@ -31,7 +31,7 @@ export class UserAuthStepDefinition extends BaseStepDefinition {
   @when(/the UI user is logged in the UI/)
   public async givenUserIsLoggedIn(): Promise<void> {
     const loginPage = new LoginPage(this.sessionContext.page ?? (() => { throw new Error('page is null'); })());
-    await loginPage.goto(process.env.HOBBYFARM_ADMIN_UI_URL as string);
+    await loginPage.goto(process.env.HOBBYFARM_UI_URL as string);
     const homePage = await loginPage.fillCredentialsAndSubmit(process.env.HOBBYFARM_UI_HEADER_TITLE as string, process.env.HOBBYFARM_ADMIN_UI_USR as string, process.env.HOBBYFARM_ADMIN_UI_PWD as string);
     this.sessionContext.current = homePage;
   }
